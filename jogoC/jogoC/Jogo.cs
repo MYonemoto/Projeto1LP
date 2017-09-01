49c35b58e19ddf9bcd099890e3b0f605445d3a57
Geovani Pedroso da Mata <geovanimata@alunos.utfpr.edu.br>
2017-09-01 17:52:35 -0300

    classe jogo

---
+++ jogoC/jogoC/Jogo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoC
+{
+    class Jogo
+    {
+        Dictionary<Sala, List<Opcao>> map;
+
+        public Jogo()
+        {
+            this.map =  new Dictionary<Sala, List<Opcao>>();
+        }
+
+
+        public Dictionary<Sala, List<Opcao>> Map
+        {
+            get { return map; }
+            set { map = value; }
+        }
+
+
+
+    }
+}
